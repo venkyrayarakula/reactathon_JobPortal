e5fde40256490f11b1fb5bb41ea4fe7745450ae7
Veekshith <veekshith59@gmail.com>
2018-07-03 11:56:55 +0000

    Graphql config

---
+++ src/global/config/index.js
@@ -0,0 +1,6 @@
+export default {
+    "graphqlEndpoint": "https://atha5xamfbds7hrsmkiefstqje.appsync-api.us-east-1.amazonaws.com/graphql",
+    "region": "us-east-1",
+    "authenticationType": "API_KEY",
+    "apiKey": "da2-wpoqr5iix5egphy5fm3hsh22he"
+}
